e9bfd79af9f1af15e325312b3ae6072c5f2eff01
mherrmannsd <mherrmannsd>
2011-05-19 11:09:05 +0000

    better layout

--- _projectCommon.php
+++ _projectCommon.php
@@ -9,10 +9,17 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
-	$Nav->addNavSeparator("Project Home", 	"downloads.php");
-	$Nav->addCustomNav("Downloads", 		"downloads.php", 	"_self", 2);
-	$Nav->addCustomNav("Installation", 		"install.php", 		"_self", 2);
-	$Nav->addCustomNav("FAQ", 				"faq.php", 			"_self", 2);
+	# $Nav->addNavSeparator("Project Home", 	"downloads.php");
+	# $Nav->addCustomNav("Downloads", 		"downloads.php", 	"_self", 2);
+	# $Nav->addCustomNav("Installation", 		"install.php", 		"_self", 2);
+	# $Nav->addCustomNav("FAQ", 				"faq.php", 			"_self", 2);
+
+	# Break the navigation into sections
+	$Nav->addNavSeparator("MyProject", 	"/default");
+	$Nav->addCustomNav("Download", "/project/download.php", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/project/documentation.php", "_blank", 3);
+	$Nav->addCustomNav("Support", "/project/support.php", "_blank", 3);
+	$Nav->addCustomNav("Getting Involved", "/project/developers", "_blank", 3);
 
 	# To define additional CSS or other pre-body headers
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>');
