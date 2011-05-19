742706ae41ed4d616c3f04d009c363e4ad46f0a0
mherrmannsd <mherrmannsd>
2011-05-19 09:53:35 +0000

    better layout

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
 	#
 	#****************************************************************************
 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());
+	
+	$localVersion = false;
+	
 	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Edapt";
