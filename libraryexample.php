02f654b3f3a4cd7447747a848b24d73a670624c3
mherrmannsd <mherrmannsd>
2011-08-14 08:24:09 +0000

    getting started with the library example

---
+++ libraryexample.php
@@ -0,0 +1,35 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+/*******************************************************************************
+ * Copyright (c) 2009 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    
+ *******************************************************************************/
+
+	$pageTitle 		= "Edapt - Library Example";
+
+	$html  = <<<EOHTML
+<div id="midcolumn">
+<h2>$pageTitle</h2>
+
+<h3>Ecore Model History</h3>
+
+<h3>Reusable Coupled Operations</h3>
+
+<h3>Custom Coupled Operations</h3>
+
+<h3>Migrator</h3>
+
+<h3>Advanced Functions</h3>
+
+<h3>Conclusion</h3>
+
+</div>
+EOHTML;
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
