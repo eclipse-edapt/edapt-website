a4c6e81cf830894e69be363f050270c4458538f0
mherrmannsd <mherrmannsd>
2011-08-14 08:43:49 +0000

    some text

--- libraryexample.php
+++ libraryexample.php
@@ -16,6 +16,39 @@
 <div id="midcolumn">
 <h2>$pageTitle</h2>
 
+<p>
+The Eclipse Modeling Framework (EMF) supports the developers to build modeling tools using metamodels in Ecore. 
+Due to the changing requirements of these modeling tools, the metamodels are often subject to change. 
+But when the metamodel changes, models created with the modeling tools often can not be loaded. 
+In order not to lose the information stored in these models, the models must be migrated.
+</p>
+<p>
+Even established frameworks such as for example the Graphical Modeling Framework (GMF) have adapted their metamodel to changing requirements in the past and therefore had to implement a migrator. 
+Conversely, not few existing modeling tools use aged metamodels that do no longer fully support the current requirements.
+The reason is the enormous effort for the migration of existing models in response to changes to the metamodel. 
+This migration is also more error prone and expensive to test.
+</p>
+<p>
+Edapt supports the developer to migrate models, by largely automating the migration. 
+Thereby it differs from languages for model transformation, which require manual specification of the migration. 
+Furthermore, in case of model transformations, one must also define rules for metamodel elements that have not changed. 
+With Edapt, however, it is sufficient to specify the migration only for changes to the metamodels.
+</p>
+<p>
+Edapt follows an operation-based approach. 
+Operations encapsulate the changes to the metamodel and the migration of models. 
+Operations can be reused to help automate the migration.
+A very simple example is an operation that renames an attribute and performs the renaming in the models. 
+Edapt currently offers over 60 reusable operations that have been proven in case studies. 
+If a migration can not be expressed by reusable operations, then it can also be manually specified for metamodel changes.
+</p>
+<p>
+Edapt is very convenient for the developer because it is integrated directly into the Ecore editor. 
+The operations can be performed directly in the editor and their execution is recorded in an explicit history. 
+To e.g. rename an attribute, the attribute is selected, the Rename operation is selected and a new name be chosen, and even surgery can be performed. 
+The history that is recorded when executing change operations can be used to automatically migrate the existing models.
+</p>
+
 <h3>Ecore Model History</h3>
 
 <h3>Reusable Coupled Operations</h3>
