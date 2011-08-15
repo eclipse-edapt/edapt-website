349281e10626c7685b62b1383d431e7cb6b31e49
mherrmannsd <mherrmannsd>
2011-08-15 07:37:44 +0000

    code snipped added

--- libraryexample.php
+++ libraryexample.php
@@ -127,6 +127,56 @@ In the case of <i>Delete Feature</i>, only the feature is deleted on the metamod
 Therefor, a helper method is used, which ensures that in case of a containment reference, also all values ​​of all children will be deleted.
 </p>
 
+<source lang="java">
+package org.eclipse.emf.edapt.declaration.creation;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edapt.declaration.EdaptOperation;
+import org.eclipse.emf.edapt.declaration.EdaptParameter;
+import org.eclipse.emf.edapt.declaration.OperationImplementation;
+import org.eclipse.emf.edapt.migration.Instance;
+import org.eclipse.emf.edapt.migration.Metamodel;
+import org.eclipse.emf.edapt.migration.Model;
+
+/**
+ * {@description}
+ * 
+ * @author herrmama
+ * @author $Author: mherrmannsd $
+ * @version $Rev: 146 $
+ * @levd.rating YELLOW Hash: EC9969F8F3F4191DB05869AB26EA6796
+ */
+@EdaptOperation(identifier = "deleteFeature2", label = "Delete Feature", description = "In the metamodel, a feature is deleted. In the model, its values are deleted, too.")
+public class DeleteFeature2 extends OperationImplementation {
+
+	/** {@description} */
+	@EdaptParameter(main = true, description = "The feature to be deleted")
+	public EStructuralFeature feature;
+
+	/** {@inheritDoc} */
+	@Override
+	public void execute(Metamodel metamodel, Model model) {
+		EClass eClass = feature.getEContainingClass();
+
+		// model migration
+		for (Instance instance : model.getAllInstances(eClass)) {
+			deleteFeatureValue(instance, feature);
+		}
+
+		// metamodel adaptation
+		metamodel.delete(feature);
+		if (feature instanceof EReference) {
+			EReference reference = (EReference) feature;
+			if (reference.getEOpposite() != null) {
+				metamodel.delete(reference.getEOpposite());
+			}
+		}
+	}
+}
+</source>
+
 <h3>Custom Coupled Operations</h3>
 
 <h3>Migrator</h3>
