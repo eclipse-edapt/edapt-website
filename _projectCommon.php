c68f0030f724c5b9f5331e23d201e4e5dc2509d3
mherrmannsd <mherrmannsd>
2011-08-16 14:22:08 +0000

    additional style added

--- _projectCommon.php
+++ _projectCommon.php
@@ -35,7 +35,7 @@
 	# $Menu->addMenuItem("Developers", "/edapt/developers", "_self");
 
 	# To define additional CSS or other pre-body headers
-	# $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>');
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>');
 	
 	# To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
 	$App->Promotion = TRUE;
