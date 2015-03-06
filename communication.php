34bdd34ed7929138d5b7554516a916f094a02623
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2015-03-06 16:26:15 +0100

    Updating getting involved

--- communication.php
+++ communication.php
@@ -27,6 +27,31 @@
 		>GIT Repository</a></b> - clone our code and contribute patches</li>
 </ul>
 
+<h3>How to setup your IDE</h3>
+<ul>
+	<li>
+		Get the latest 
+		<a href="https://eclipse.org/downloads/">Eclipse Modeling Tools</a>
+		for your platform.
+	</li>
+	<li>
+		Download the Zip of the latest Edapt release from the 
+		<a href="https://www.eclipse.org/edapt/download.php">download section</a>
+	</li>
+	<li>
+		In your IDE go to Preferences => Plug-In Development => API Baselines and set the unzipped Edapt release as the API Baseline.
+	</li>
+	<li>
+		<p>Check out the Edapt source code from</p>
+		<p><code>git://git.eclipse.org/gitroot/edapt/org.eclipse.emf.edapt.git</code> (anonymous)</p>
+		<p>or</p>
+		<p><code>ssh://committer_id@git.eclipse.org:29418/edapt/org.eclipse.emf.edapt</code> (Gerrit)</p>
+	</li>
+	<li>
+		Import the source code and set the edapt_with_cdo.target target.
+	</li>
+</ul>
+
 </div>
 EOHTML;
 	# Generate the web page
