772ef97b7ab7a85bb634da5c41f7be9fb84265c3
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2019-02-26 09:22:36 +0100

    Add 1.4.0 MS1

--- download.php
+++ download.php
@@ -76,16 +76,16 @@ Edapt is provided as a P2 repository URL. This URL can be pasted into the Eclips
 	<td width="100"><b>1.0.x</b></td>
 	<td width="200"><a href="http://download.eclipse.org/edapt/releases/10x">P2 Composite Site</a></td>
 </tr>
-<!--tr>
+<tr>
 	<td>Milestones</td>
-	<td><b>1.3.0</b></td>
-	<td><a href="http://download.eclipse.org/edapt/releases/13x">P2 Composite Site</a></td>
+	<td><b>1.4.0</b></td>
+	<td><a href="http://download.eclipse.org/edapt/releases/14x">P2 Composite Site</a></td>
 </tr>
 <tr>
 	<td></td>
-	<td>1.3.0 RC1</td>
-	<td><a href="http://download.eclipse.org/edapt/releases/13x/130RC1">P2 Site</a></td>
-</tr-->
+	<td>1.4.0 MS1</td>
+	<td><a href="http://download.eclipse.org/edapt/releases/14x/140MS1">P2 Site</a></td>
+</tr>
 <tr>
 	<td>Nightly</td>
 	<td>1.4.0</td>
