86ea6a22c7c60ad7e9c5defa48c03787d465ea60
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2022-03-30 11:13:22 +0200

    Edapt 1.5.0 RC1
    Change-Id: Ide894702fb050c34fb10e327cc354e5a5b2413c0

--- download.php
+++ download.php
@@ -83,14 +83,14 @@ Edapt is provided as a P2 repository URL. This URL can be pasted into the Eclips
 	<td width="200"><a href="http://download.eclipse.org/edapt/releases/10x">P2 Composite Site</a></td>
 </tr>
 <tr>
-	<td>Milestones</td>
+	<td>Release Candidates</td>
 	<td><b>1.5.x</b></td>
 	<td><a href="http://download.eclipse.org/edapt/releases/15x">P2 Composite Site</a></td>
 </tr>
 <tr>
 	<td></td>
-	<td>1.5.0 MS1</td>
-	<td><a href="http://download.eclipse.org/edapt/releases/15x/150MS1">P2 Site</a></td>
+	<td>1.5.0 RC1</td>
+	<td><a href="http://download.eclipse.org/edapt/releases/15x/150RC1">P2 Site</a></td>
 </tr>
 <tr>
 	<td>Nightly</td>
